801dbdab790d1de69eee0eff86cc94555927247a
FernandoDantas <fernandoalbino755@gmail.com>
2017-06-26 10:51:58 -0400

    update github.io

--- README.md
+++
@@ -1,6 +0,0 @@
-### Flappy Bird
-
-Game produced in the Construct 2 tool in the postgraduate course in the development of electronic games at the University of the State of Amazonas - UEA.
-
---
-MIT Licensed.
---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1498488479,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/spritreference-sheet0.png",
+		"images/background.png",
+		"images/ground.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"media/birdfail.ogg",
+		"media/birdisdead.ogg",
+		"media/jumpbird.ogg",
+		"media/score.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
